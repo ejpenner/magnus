b58936ce36a687ba541fc919ca272249bd74fb88
Eric Penner <epenner@unomaha.edu>
2016-06-08 21:50:53 +0000

    More messing around with CSS

--- resources/views/home/recent.blade.php
+++ resources/views/home/recent.blade.php
@@ -5,7 +5,9 @@
         <p>Recently Updated Galleries</p>
         @if(\App\Gallery::count() > 0)
             <ul>
-
+                @foreach($galleries as $gallery)
+                    <li>{{ $gallery->name }}</li>
+                @endforeach
             </ul>
         @endif
     </div>
@@ -15,9 +17,11 @@
                 <div class="row">
                     @foreach($pieceChunk as $piece)
                         <div class="col-md-3 vcenter gallery-item">
+                            <div class="">
                             <a href="{{ action('PieceController@show', [$piece->featured->first()->gallery_id, $piece->id]) }}">
                                 <img src="/{{ $piece->getThumbnail() }}" alt="">
                             </a>
+                            </div>
                             <h4><a href="{{ action('PieceController@show', [$piece->featured->first()->gallery_id, $piece->id]) }}">{{ $piece->title }}</a> -
                                 <small><a href="{{ action('ProfileController@show', $piece->user->slug) }}">{{ $piece->user->name }}</a></small></h4>
                         </div>
