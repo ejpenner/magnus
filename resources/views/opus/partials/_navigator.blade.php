f9355689d091dc09f3aa09c6383ab58e10ea3226
Eric Penner <epenner@unomaha.edu>
2016-07-06 21:56:18 +0000

    Refactored more $opus->id to $opus->slug. Added a partial to opus.show that lists all the galleries it is in

---
+++ resources/views/opus/partials/_navigator.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: epenner
+ * Date: 7/6/2016
+ * Time: 4:17 PM
+ */
---
+++ resources/views/partials/_operationsDropdownSlug.blade.php
@@ -0,0 +1,13 @@
+<div class="btn-group">
+    <button type="button" class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Actions <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu">
+        {!! Form::model($model, ['method'=>'delete', 'class'=>'delete-confirm operations',
+                                       'action'=>[$controller.'@destroy', $model->slug]]) !!}
+        <li><a class="btn btn-link" href="{{ action($controller.'@edit', [$model->slug]) }}">Edit</a></li>
+        <li><button type="submit" class="btn btn-link"><i class="fa fa-trash"></i> Delete</button></li>
+        <li><a class="btn btn-link" href="">Report</a></li>
+        {!! Form::close() !!}
+    </ul>
+</div>
